9b87470e3603fcc66fdfdb78194e56749cc6909b
LenaKyrychenko <lenastanis@gmail.com>
2020-09-21 15:56:50 +0300

    add spaces

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api', function (req, res) {
-res.send('API is running');
+	res.send('API is running');
 });
 
 app.get('/ErrorExample', function(req, res, next) {
@@ -91,22 +91,22 @@ app.put('/api/articles/:id', function (req, res) {
 		article.description = req.body.description;
 		article.author = req.body.author;
 		article.images = req.body.images;
-	return article.save(function (err) {
-		if (!err) {
-			log.info("article updated");
-			return res.send({ status: 'OK', article:article });
-		} else {
-			if(err.name == 'ValidationError') {
-				res.statusCode = 400;
-				res.send({ error: 'Validation error' });
+		return article.save(function (err) {
+			if (!err) {
+				log.info("article updated");
+				return res.send({ status: 'OK', article:article });
 			} else {
-				res.statusCode = 500;
-				res.send({ error: 'Server error' });
+				if(err.name == 'ValidationError') {
+					res.statusCode = 400;
+					res.send({ error: 'Validation error' });
+				} else {
+					res.statusCode = 500;
+					res.send({ error: 'Server error' });
+				}
+				log.error('Internal error(%d): %s',
+				res.statusCode, err.message);
 			}
-			log.error('Internal error(%d): %s',
-			res.statusCode, err.message);
-		}
-	});
+		});
 	});
 });
 
@@ -116,17 +116,17 @@ app.delete('/api/articles/:id', function (req, res){
 			res.statusCode = 404;
 			return res.send({ error: 'Not found' });
 		}
-	return article.remove(function (err) {
-	if (!err) {
-		log.info("article removed");
-		return res.send({ status: 'OK' });
-	} else {
-		res.statusCode = 500;
-		log.error('Internal error(%d): %s',
-		res.statusCode, err.message);
-		return res.send({ error: 'Server error' });
-	}
-	});
+		return article.remove(function (err) {
+			if (!err) {
+				log.info("article removed");
+				return res.send({ status: 'OK' });
+			} else {
+				res.statusCode = 500;
+				log.error('Internal error(%d): %s',
+				res.statusCode, err.message);
+				return res.send({ error: 'Server error' });
+			}
+		});
 	});
 });
 
